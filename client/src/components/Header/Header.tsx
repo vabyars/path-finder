966d515645ba26cab94a4b4d238f817d579a4af7
vabyars <vasiliyrait@gmail.com>
2021-04-26 13:45:46 +0500

    Get current algorithms and mazes from server

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,32 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button} from '@skbkontur/react-ui'
 import './Header.css'
 import { strict } from 'node:assert'
-// import Select from "react-dropdown-select";
 import Select from 'react-select';
 
 
-const algorithms =[
-    { label: "A*", value: 1 },
-    { label: "IDA*", value: 2 },
-    { label: "Dijkstra", value: 3 },
-    { label: "BFS", value: 4 },
-    { label: "DFS", value: 5 },
-]
+interface SelectData{
+    label: string,
+    value: number
+}
+
+function parseArrayToSelectData(data: any[]){
+    return data.map(function(label, i){return {label: label, value: i + 1}})
+}
 
 
 function Header(props: any){
-    const [currentAlgorithm, setAlgorithm] = useState(algorithms[0])
+    const [algorithms, setAlgorithms] = useState<SelectData[]>([])
+    const [currentAlgorithm, setAlgorithm] = useState<SelectData>()
+    const [mazes, setMazes] = useState<SelectData[]>([])
+    const [currentMaze, setMaze] = useState<SelectData>()
+    
+
+
+    async function loadMazesAndAlgorithms(){
+        let data = await( await fetch("/settings")).json()
+        let parsedAlgorithms = parseArrayToSelectData(data.algorithms)
+        let parsedMazes = parseArrayToSelectData(data.mazes)
+        setMazes(parsedMazes)
+        setAlgorithms(parsedAlgorithms)
+        setAlgorithm(parsedAlgorithms[0])
+        setMaze(parsedMazes[0])
+      }
+
+    useEffect(() => {
+         loadMazesAndAlgorithms()
+        }, [])
 
     return (
         <div className="header">
             <label className="flex-elem logo">Pathfinder</label>
 
-            <Button className="flex-elem"> Start</Button>
+            <Button className="flex-elem" onClick={() => console.log(currentMaze)}> Start</Button>
             <Button className="flex-elem"> Pause</Button>
             <Button className="flex-elem" onClick={props.clearFunc}> Clear field</Button>
 
-            <Select className="flex-elem algorithm" isSearchable={false}  options={algorithms} defaultValue={currentAlgorithm}  onChange={(value: any) => setAlgorithm(value)} />
+            { currentAlgorithm && <Select className="flex-elem algorithm" isSearchable={false}
+              options={algorithms} defaultValue={currentAlgorithm}  onChange={(value: any) => setAlgorithm(value)} />}
+            { currentMaze && <Select className="flex-elem mazes" isSearchable={false}
+              options={mazes} defaultValue={currentMaze}  onChange={(value: any) => setMaze(value)} /> }
         </div>        
     )
 }
