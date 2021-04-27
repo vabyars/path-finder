5af9b3d74d833a2ba667fd04555bbef21161a8a6
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-04-27 15:34:58 +0500

    add Diagonal Jump Point Search

---
+++ server/PathFinder.Domain/Models/Algorithms/JPS/JumpPointSearchState.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+using PathFinder.Domain.Interfaces;
+using PathFinder.Domain.Models.States;
+
+namespace PathFinder.Domain.Models.Algorithms.JPS
+{
+    public class JumpPointSearchState : State
+    {
+        public Point Point { get; }
+        
+        public JumpPointSearchState(Point point)
+        {
+            Point = point;
+        }
+    }
+}
