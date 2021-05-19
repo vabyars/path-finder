de590e4c6109b67b0839ce8c57ce9d7735430d35
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-05-20 01:45:49 +0500

    add test to heap and dictionary priority queues.

---
+++ server/PathFinder.Test/InfrastructureTest/QueueProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using PathFinder.Infrastructure;
+
+namespace PathFinder.Test.InfrastructureTest
+{
+    public class QueueProvider
+    {
+        [Test]
+        public void Heap()
+        {
+            Func<HeapPriorityQueue<int>> getInstance = () => new HeapPriorityQueue<int>();
+            new PriorityQueueTest().Run(getInstance);
+        }
+        
+        [Test]
+        public void Dictionary()
+        {
+            Func<DictionaryPriorityQueue<int>> getInstance = () => new DictionaryPriorityQueue<int>();
+            new PriorityQueueTest().Run(getInstance);
+        }
+    }
+}
