75fa7504340b5d1886c53a370997f0a4c168121a
Danil <danil.lunyow@mail.ru>
2021-06-17 18:59:42 +0500

    add mazeService tests

--- server/PathFinder.Test/GridTest/TestGrid.cs
+++ server/PathFinder.Test/GridTest/TestGrid.cs
@@ -8,14 +8,6 @@ namespace PathFinder.Test.GridTest
     [TestFixture]
     public class TestGrid
     {
-        private Grid grid;
-        
-        [SetUp]
-        public void SetUp()
-        {
-            grid = new Grid(new[,] {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}});
-        }
-        
         [Test]
         [TestCase(0, 0, true)]
         [TestCase(1, 1, true)]
@@ -27,6 +19,8 @@ namespace PathFinder.Test.GridTest
         [TestCase(10, 10, false)]
         public void TestInBounds(int x, int y, bool expected)
         {
+            var grid = GetDefault3X3Grid();
+            
             Assert.AreEqual(expected, grid.InBounds(x, y));
         }
 
@@ -38,7 +32,7 @@ namespace PathFinder.Test.GridTest
         [TestCase(1, 0, false)]
         public void TestIsPassable(int x, int y, bool expected)
         {
-            grid = new Grid(new[,] {{10, 0, -1}, {-100, 1, 1}, {1, 1, 1}});
+            var grid = new Grid(new[,] {{10, 0, -1}, {-100, 1, 1}, {1, 1, 1}});
             
             Assert.AreEqual(expected, grid.IsPassable(x, y));
         }
@@ -46,6 +40,8 @@ namespace PathFinder.Test.GridTest
         [Test]
         public void TestGetCost()
         {
+            var grid = GetDefault3X3Grid();
+            
             Assert.AreEqual(1, grid.GetCost(new Point(0, 0), new Point(1, 0)));
             Assert.AreEqual(Math.Sqrt(2), grid.GetCost(new Point(0, 0), new Point(1, 1)), 0.001);
         }
@@ -53,7 +49,7 @@ namespace PathFinder.Test.GridTest
         [Test]
         public void TestGetCost2()
         {
-            grid = new Grid(new[,] {{2, 2, 2}, {2, 2, 2}, {2, 2, 2}});
+            var grid = new Grid(new[,] {{2, 2, 2}, {2, 2, 2}, {2, 2, 2}});
             
             Assert.AreEqual(2, grid.GetCost(new Point(0, 0), new Point(1, 0)));
             Assert.AreEqual(Math.Sqrt(2) * 2, grid.GetCost(new Point(0, 0), new Point(1, 1)), 0.001);
@@ -62,6 +58,8 @@ namespace PathFinder.Test.GridTest
         [Test]
         public void TestGetNeighborsWithIncorrectPoints()
         {
+            var grid = GetDefault3X3Grid();
+            
             CollectionAssert.AreEquivalent(Array.Empty<Point>(), 
                 grid.GetNeighbors(new Point(-1, -1), false));
             
@@ -72,6 +70,8 @@ namespace PathFinder.Test.GridTest
         [Test]
         public void TestGetNeighborsWithCenterPoint()
         {
+            var grid = GetDefault3X3Grid();
+            
             CollectionAssert.AreEquivalent(new Point[]{new (0, 1), new (1, 0), new(1, 2), new(2, 1)}, 
                 grid.GetNeighbors(new Point(1, 1), false));
             
@@ -84,6 +84,8 @@ namespace PathFinder.Test.GridTest
         [Test]
         public void TestGetNeighborsWithCornerPoint()
         {
+            var grid = GetDefault3X3Grid();
+            
             CollectionAssert.AreEquivalent(new Point[]{new(0, 1), new(1, 0)}, 
                 grid.GetNeighbors(new Point(0, 0), false));
             
@@ -94,7 +96,7 @@ namespace PathFinder.Test.GridTest
         [Test]
         public void TestGetNeighborsWithWall()
         {
-            grid = new Grid(new[,] {{1, -1, 1}, {1, 1, 1}, {1, 1, 1}});
+            var grid = new Grid(new[,] {{1, -1, 1}, {1, 1, 1}, {1, 1, 1}});
             
             CollectionAssert.AreEquivalent(new Point[]{new(1, 0)}, 
                 grid.GetNeighbors(new Point(0, 0), false));
@@ -109,7 +111,7 @@ namespace PathFinder.Test.GridTest
             // --8
             // -8x
             // ---
-            grid = new Grid(new[,] {{1, 1, 1}, {1, -1, 1}, {1, 1, -1}});
+            var grid = new Grid(new[,] {{1, 1, 1}, {1, -1, 1}, {1, 1, -1}});
             
             CollectionAssert.AreEquivalent(new Point[]{new(0, 2)}, 
                 grid.GetNeighbors(new Point(1, 2), false));
@@ -117,5 +119,7 @@ namespace PathFinder.Test.GridTest
             CollectionAssert.AreEquivalent(new Point[]{new(0, 2), new(0, 1)}, 
                 grid.GetNeighbors(new Point(1, 2), true));
         }
+        
+        private Grid GetDefault3X3Grid() => new Grid(new[,] {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}});
     }
 }
